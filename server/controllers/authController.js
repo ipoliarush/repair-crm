1ae2167a6e99a7e28c05c09efb10cab983bb6446
raizoketh <enridefi221@gmail.com>
2026-03-19 22:14:38 +0100

    add login and jwt authentication

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js"
 import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
 
 export const register = async (req, res) => {
   try {
@@ -20,6 +21,35 @@ export const register = async (req, res) => {
     })
 
     res.json(user)
+
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body
+
+    const user = await User.findOne({ email })
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" })
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" })
+    }
+
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    )
+
+    res.json({ token })
 
   } catch (error) {
     res.status(500).json({ error: error.message })
