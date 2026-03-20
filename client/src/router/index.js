d78d244f78fb2279e3d25245527e39ae614af807
raizoketh <enridefi221@gmail.com>
2026-03-20 23:23:53 +0100

    add orders page with protected route

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,12 +2,24 @@ import { createRouter, createWebHistory } from "vue-router"
 import Login from "../views/Login.vue"
 
 const routes = [
-  { path: "/login", component: Login }
+  { path: "/login", component: Login },
+  { path: "/orders", component: () => import("../views/Orders.vue") }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+// 🔐 guard
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem("token")
+
+  if (to.path !== "/login" && !token) {
+    return next("/login")
+  }
+
+  next()
 })
 
 export default router
---
+++ client/src/views/Orders.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref, onMounted } from "vue"
+import api from "../api/api"
+
+const orders = ref([])
+
+const loadOrders = async () => {
+  const res = await api.get("/orders")
+  orders.value = res.data
+}
+
+onMounted(loadOrders)
+</script>
+
+<template>
+  <div>
+    <h1>Orders</h1>
+
+    <div v-for="order in orders" :key="order._id">
+      <p><b>{{ order.clientName }}</b></p>
+      <p>{{ order.deviceType }}</p>
+      <p>{{ order.status }}</p>
+      <hr />
+    </div>
+  </div>
+</template>
